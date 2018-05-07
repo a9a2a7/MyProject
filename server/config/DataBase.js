457198603bfe24b462cb0c9fcdfe049a235abb77
a9a2a7 <38461253+a9a2a7@users.noreply.github.com>
2018-05-07 16:20:47 +0000

    Update DataBase.js

--- server/config/DataBase.js
+++ server/config/DataBase.js
@@ -1,4 +1,4 @@
 var mongoose = require('mongoose');
-var connection = mongoose.connect('mongodb://localhost/People_db');
+var connection = mongoose.connect('mongodb://a9a2a7:a@92102207@ds217310.mlab.com:17310/people');
  
 module.exports = connection;
