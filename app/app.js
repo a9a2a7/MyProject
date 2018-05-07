691357a293827bad12f9542b499984e950b85ec7
a9a2a7 <38461253+a9a2a7@users.noreply.github.com>
2018-05-07 11:29:52 +0300

    last

--- app/app.js
+++ app/app.js
@@ -11,24 +11,12 @@
     url: "/",
     templateUrl: "/views/people/index.html",
     controller: "peopleController"
-    }).state("create", {
-    url: "/create",
-    templateUrl: "/views/people/create.html",
-    controller: "peopleController"
-    }).state("edit", {
-    url: "/edit/:id",
-    templateUrl: "/views/people/create.html",
-    controller: "peopleController"
-    }).state("details", {
-    url: "/details/:id",
-    templateUrl: "/views/people/details.html",
-    controller: "peopleController"
     }).state("register", {
         url: "/register/",
         templateUrl: "/views/login/register.html"
         
     }).state("login", {
-        url: "/login/",
+        url: "/login",
         templateUrl: "/views/login/login.html"
         });
     })
--- app/controllers/people.controller.js
+++ app/controllers/people.controller.js
@@ -18,6 +18,14 @@
     console.log(err);
     });
     
+    $scope.EditPeople = function(id) {
+    peopleService.getPeople(id).then(function(res) {
+        $scope.people = res.data;
+        }).catch(function(err) {
+        console.log(err);
+        });
+    };
+
     $scope.deletePeople = function(id) {
     if (confirm('Are you sure to delete?')) {
     peopleService.deletePeople(id).then(function(res) {
@@ -29,40 +37,32 @@
     });
     }
     };
-    } else if ($state.current.name == "edit") {
-    $rootScope.Title = "Edit People";
-    var id = $stateParams.id;
-    peopleService.getPeople(id).then(function(res) {
-    $scope.people = res.data;
-    }).catch(function(err) {
-    console.log(err);
-    });
-    
-    $scope.saveData = function(people) {
-    if ($scope.peopleForm.$valid) {
-    peopleService.updatePeople(people).then(function(res) {
-    if (res.data == "updated") {
-    $state.go("peoples");
-    }
-    }).catch(function(err) {
-    console.log(err);
-    });
-    }
-    };
-    } else if ($state.current.name == "create") {
-    $rootScope.Title = "Create People";
+
     $scope.saveData = function(people) {
     $scope.IsSubmit = true;
     if ($scope.peopleForm.$valid) {
     peopleService.createPeople(people).then(function(res) {
     if (res.data == "created") {
-    $state.go("peoples");
+        $state.go("peoples", {}, { reload: true });
     }
     }).catch(function(err) {
     console.log(err);
     });
+
+    peopleService.updatePeople(people).then(function(res) {
+        if (res.data == "updated") {
+            $state.go("peoples", {}, { reload: true });
+        }
+        }).catch(function(err) {
+        console.log(err);
+        });
+
+
     }
     };
-    }
+    
+
+
+    }  
     }
    })();
